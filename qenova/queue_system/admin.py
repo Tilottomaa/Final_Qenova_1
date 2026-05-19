e893b1b164086c1267c4c83732fa3f64505fc245
Fahim Azad <140386991+fahim-azad@users.noreply.github.com>
2026-05-20 01:13:50 +0600

    model admin view

--- qenova/queue_system/admin.py
+++ qenova/queue_system/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Organization, QueueBooking, Token, Feedback
 
-# Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'account', 'type', 'token_limit', 'queue_status')
+
+@admin.register(QueueBooking)
+class QueueBookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'organization', 'booking_date', 'queue_position')
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('serial_number', 'user', 'organization', 'status', 'booking_date')
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'organization_id', 'rating', 'submitted_at')
+    list_filter = ('rating', 'submitted_at')
